c51dccb85e073c1445d10d5a5a3682e339ade414
trieulh <trieulh.dangky@gmail.com>
2012-07-03 14:54:42 +0700

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnapA::Application.config.secret_token = '7421e9209268bf09e7704b1f6ebebb7d1aa3b6971c430916bc3364492df9cbeb6453750bd55092943038b1b3c01d42cdf27f3f20641d4e52b31fe63f5c2a9f1e'
