ade440f9aff3e674218dfee663945ddbdd04d6f4
KienBui <doankien.bui@gmail.com>
2012-07-09 09:08:56 +0700

    Sua ti

--- db/migrate/20120703145150_add_persistence_token_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddPersistenceTokenToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :persistence_token, :string
-    add_column :users, :single_access_token, :string
-    add_index  :users, :persistence_token
-    add_index  :users, :single_access_token
-  end
-end
