2ccb21b3fe54fe0310793993c655ad7a12138e03
KienBui <doankien.bui@gmail.com>
2012-07-10 11:29:51 +0700

    up buoi trua

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report < ActiveRecord::Base
-  attr_accessible :source_id, :target_id, :relation_type
+  attr_accessible :source_id, :target_id, :title
 
   validates :source_id,presence: true
   validates :target_id,presence: true
