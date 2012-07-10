43ccb9927646ec7d336d4ff789b77a31fb860b0c
trieulh <trieulh.dangky@gmail.com>
2012-07-10 15:44:07 +0700

    fix popup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,9 @@ SnapA::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
 
 end
