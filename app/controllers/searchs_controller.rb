693309b064261c5b16761ee667a0901d2f329df0
KienBui <doankien.bui@gmail.com>
2012-07-11 11:03:06 +0700

    up buoi sang

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -3,7 +3,7 @@ class SearchsController < ApplicationController
     @results = []
     @results.clear
     Photo.all.each do |photo|
-      if photo.name.include? params[:search]
+      if photo.name.downcase.include? params[:search].downcase
         @results.push photo
       end
     end
@@ -14,7 +14,7 @@ class SearchsController < ApplicationController
     @results = []
     @results.clear
     User.all.each do |user|
-      if user.name.include? params[:search]
+      if user.name.downcase.include? params[:search].downcase
         @results.push user
       end
     end
@@ -25,7 +25,7 @@ class SearchsController < ApplicationController
     @results = []
     @results.clear
     Box.all.each do |box|
-      if box.name.include? params[:search]
+      if box.name.downcase.include? params[:search].downcase
         @results.push box
       end
     end
